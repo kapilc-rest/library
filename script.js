bbc3046dc1e68e0aaf78757f0b8bb4ed71386fe2
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-09-21 07:11:28 +0000

    Add addBookLibrary function

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const library = [];
+
 function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    function info() {
+    this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
     }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    return library.push(newBook);
 }
